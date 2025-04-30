e4bdc2499e8c1f58eec2a272b917f2a763b25153
lanly-dev-deploy <lanly-dev@deploy.noreply.github.com>
2025-04-30 19:09:47 +0000

    deploy: lanly-dev/website-vitepress@fce8c59379d9533964747fe3a664131a9f1cf1ea

--- assets/index.md.B_YELRg1.js
+++
@@ -1,6 +0,0 @@
-import{_ as t,c as a,o as n,j as o}from"./chunks/framework.DPDPlp3K.js";const f=JSON.parse('{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home","sidebar":false},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),r={name:"index.md"};function s(i,e,d,l,p,m){return n(),a("div",null,e[0]||(e[0]=[o("pre",{id:"intro"},`Hello, welcome to my website 👋
-
-I'm a software developer and interest in and passionate about learning the new things
-
-Currently, I'm on the full-stack development path and work mainly with Javascript, Typescript, Nodejs, Java and Vuejs
-`,-1)]))}const u=t(r,[["render",s]]);export{f as __pageData,u as default};
